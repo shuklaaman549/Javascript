71bea63f790402b13df3fb9d0d59749be3394acb
Aman Shukla <shuklaaman549@gmail.com>
2025-04-30 22:58:23 +0000

    Learned dataTypes

---
+++ 01_JS/dataTypes.js
@@ -0,0 +1,33 @@
+
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Aman")
+
+
+let myName = "Ammyyy"
+let age = 23
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof myName);
+console.log(typeof age);
+console.log(typeof "Ammyyy");
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
