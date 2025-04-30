737a6a7737723a06b43d2da9256553bdcb8c12da
Aman Shukla <shuklaaman549@gmail.com>
2025-04-30 23:17:11 +0000

    Learned data conversion

---
+++ 01_JS/dataConversion.js
@@ -0,0 +1,30 @@
+let score = "Ammyyy"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log(typeof score);
+
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "Aman"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Aman" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
