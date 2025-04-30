923c19ee1e2268dd91218edbe0b1b2df032b9eb5
Aman Shukla <shuklaaman549@gmail.com>
2025-04-30 22:34:21 +0000

    Learned constant and variables

---
+++ 01_JS/variables.js
@@ -0,0 +1,13 @@
+const helloAman = 123
+let uniqueId = 5678
+var letMeCheck = "Yayyy"
+
+hmmOk = "IDK"
+uniqueId = 6333
+letMeCheck = "Nooo"
+
+hmmOk = "IK"
+let ekAur
+// helloAman = 222
+
+console.table([helloAman, uniqueId, letMeCheck, hmmOk, ekAur])
